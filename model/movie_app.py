92f20fc261dbd18e7f7f852954b2a8e15b2d29ef
aamadasun1 <tamadasun@gmail.com>
2024-02-14 14:01:40 -0500

    need review

--- model/movie_app.py
+++ model/movie_app.py
@@ -39,47 +39,51 @@ def display_movie_info(movie_data):
 
 def app():
     st.title("The Cinematic Nexus: Unveiling the Future of Movie Recommendations and Analysis")
-    
+
     # Welcome Section
     st.write(
         "Welcome to The Cinematic Nexus! Discover your next favorite movie with our advanced recommendation system."
     )
-    
+
     # Sidebar
     st.sidebar.header("User Preferences")
     user_input_movie = st.sidebar.text_input("Enter your favorite movie:")
-    
+
     if st.sidebar.button("Get Recommendations"):
         if user_input_movie:
-            recommended_movies = get_movie_recommendations(user_input_movie, item_similarity_matrix, interaction_data_pivot)
-            st.subheader(f"Recommended Movies based on '{user_input_movie}':")
-            display_movie_info(tmdb_df[tmdb_df['title'].isin(recommended_movies[:5])])
-        else:
-            st.warning("Please enter a movie title.")
-            
+            try:
+                recommended_movies = get_movie_recommendations(user_input_movie, item_similarity_matrix, interaction_data_pivot)
+                st.subheader(f"Recommended Movies based on '{user_input_movie}':")
+                display_movie_info(tmdb_df[tmdb_df['title'].isin(recommended_movies[:5])])
+            except KeyError:
+                st.warning("Movie not found in the dataset. Please enter a new movie.")
+                return
+
     st.sidebar.header("User Preferences (Advanced)")
     user_preference = st.sidebar.text_input("Enter your favorite genre, director, or actor/actress:")
     start_year = st.sidebar.number_input("Enter the starting year:", 1900, 2022, 1900)
     end_year = st.sidebar.number_input("Enter the ending year:", 1900, 2022, 2022)
-    
+
     if st.sidebar.button("Advanced Recommendations"):
         if user_preference:
-            filtered_movies = tmdb_df[
-                (tmdb_df['genre_names'].str.lower().str.contains(user_preference.lower())) |
-                (tmdb_df['directors'].str.lower().str.contains(user_preference.lower())) |
-                (tmdb_df['cast'].str.lower().str.contains(user_preference.lower())) &
-                (tmdb_df['release_year'].between(start_year, end_year))
-            ]
+            try:
+                filtered_movies = tmdb_df[
+                    (tmdb_df['genre_names'].str.lower().str.contains(user_preference.lower())) |
+                    (tmdb_df['directors'].str.lower().str.contains(user_preference.lower())) |
+                    (tmdb_df['cast'].str.lower().str.contains(user_preference.lower())) &
+                    (tmdb_df['release_year'].between(start_year, end_year))
+                ]
 
-            if not filtered_movies.empty:
-                sorted_movies = filtered_movies.sort_values(by='popularity_normalized', ascending=False)
-                recommended_movies = sorted_movies['title'].tolist()
-                st.subheader("Recommended Movies based on Advanced Preferences:")
-                display_movie_info(tmdb_df[tmdb_df['title'].isin(recommended_movies[:5])])
-            else:
-                st.warning("No movies found based on your advanced preferences.")
-        else:
-            st.warning("Please enter a genre, director, or actor/actress for advanced recommendations.")
+                if not filtered_movies.empty:
+                    sorted_movies = filtered_movies.sort_values(by='popularity_normalized', ascending=False)
+                    recommended_movies = sorted_movies['title'].tolist()
+                    st.subheader("Recommended Movies based on Advanced Preferences:")
+                    display_movie_info(tmdb_df[tmdb_df['title'].isin(recommended_movies[:5])])
+                else:
+                    st.warning("No movies found based on your advanced preferences.")
+            except KeyError:
+                st.warning("Movie not found in the dataset. Please enter a new movie.")
+                return
 
 if __name__ == "__main__":
     app()
