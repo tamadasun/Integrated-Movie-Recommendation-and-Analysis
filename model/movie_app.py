b2aef4665d4459c42d7d2f0646f79e6d23dac355
aamadasun1 <tamadasun@gmail.com>
2024-02-15 00:44:49 -0500

    2/15/2024

--- model/movie_app.py
+++ model/movie_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import base64
 
 from movie_recommendation import recommend_based_on_user_preference, get_movie_recommendations, \
     interactive_movie_recommendation, get_wordnet_pos, lemmatize_text, apply_tfidf_vectorizer, \
@@ -17,6 +18,7 @@ import nltk
 from nltk.corpus import wordnet
 
 
+# Load the movie dataset
 tmdb_df = pd.read_csv('../data/tmdb_data.csv')
 
 def display_movie_info(movie_data):
@@ -25,11 +27,11 @@ def display_movie_info(movie_data):
         return
     
     for index, movie in movie_data.iterrows():
-        st.write(f"Title: {movie['title']}")
-        st.write(f"Release Year: {movie['release_year']}")
-        st.write(f"Genres: {movie['genre_names']}")
-        st.write(f"Overview: {movie['overview']}")
-        st.write(f"Popularity: {movie['popularity']}")
+        st.write(f"**Title:** {movie['title']}")
+        st.write(f"**Release Year:** {movie['release_year']}")
+        st.write(f"**Genres:** {movie['genre_names']}")
+        st.write(f"**Overview:** {movie['overview']}")
+        st.write(f"**Popularity:** {movie['popularity']}")
         
         poster_url = f"https://image.tmdb.org/t/p/original{movie['poster_path']}"
         
@@ -39,22 +41,23 @@ def display_movie_info(movie_data):
 
 def app():
     st.title("The Cinematic Nexus: Unveiling the Future of Movie Recommendations and Analysis")
-
+    
     # Welcome Section
     st.write(
         "Welcome to The Cinematic Nexus! Discover your next favorite movie with our advanced recommendation system."
     )
-
+    
     # Sidebar
     st.sidebar.header("User Preferences")
     user_input_movie = st.sidebar.text_input("Enter your favorite movie:")
-
+    
     if st.sidebar.button("Get Recommendations"):
         if user_input_movie:
             try:
-                recommended_movies = get_movie_recommendations(user_input_movie, item_similarity_matrix, interaction_data_pivot)
-                st.subheader(f"Recommended Movies based on '{user_input_movie}':")
-                display_movie_info(tmdb_df[tmdb_df['title'].isin(recommended_movies[:5])])
+                # Collaborative Filtering
+                recommended_movies_collaborative = get_movie_recommendations(user_input_movie, item_similarity_matrix, interaction_data_pivot)
+                st.subheader(f"Recommended Movies based on '{user_input_movie}' (Collaborative Filtering):")
+                display_movie_info(tmdb_df[tmdb_df['title'].isin(recommended_movies_collaborative[:5])])
             except KeyError:
                 st.warning("Movie not found in the dataset. Please enter a new movie.")
                 return
@@ -63,22 +66,23 @@ def app():
     user_preference = st.sidebar.text_input("Enter your favorite genre, director, or actor/actress:")
     start_year = st.sidebar.number_input("Enter the starting year:", 1900, 2022, 1900)
     end_year = st.sidebar.number_input("Enter the ending year:", 1900, 2022, 2022)
-
+    
     if st.sidebar.button("Advanced Recommendations"):
         if user_preference:
             try:
-                filtered_movies = tmdb_df[
+                # Content-Based Filtering
+                filtered_movies_content_based = tmdb_df[
                     (tmdb_df['genre_names'].str.lower().str.contains(user_preference.lower())) |
                     (tmdb_df['directors'].str.lower().str.contains(user_preference.lower())) |
                     (tmdb_df['cast'].str.lower().str.contains(user_preference.lower())) &
                     (tmdb_df['release_year'].between(start_year, end_year))
                 ]
 
-                if not filtered_movies.empty:
-                    sorted_movies = filtered_movies.sort_values(by='popularity_normalized', ascending=False)
-                    recommended_movies = sorted_movies['title'].tolist()
-                    st.subheader("Recommended Movies based on Advanced Preferences:")
-                    display_movie_info(tmdb_df[tmdb_df['title'].isin(recommended_movies[:5])])
+                if not filtered_movies_content_based.empty:
+                    sorted_movies_content_based = filtered_movies_content_based.sort_values(by='popularity_normalized', ascending=False)
+                    recommended_movies_content_based = sorted_movies_content_based['title'].tolist()
+                    st.subheader("Recommended Movies based on Advanced Preferences (Content-Based Filtering):")
+                    display_movie_info(filtered_movies_content_based[filtered_movies_content_based['title'].isin(recommended_movies_content_based[:5])])
                 else:
                     st.warning("No movies found based on your advanced preferences.")
             except KeyError:
